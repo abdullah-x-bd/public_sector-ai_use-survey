9f62ff50e59fc855fdf33a82ddb7b57f4ab03ce7
Abdullah X <ax.zephara@gmail.com>
2026-08-30 06:56:38 +0530

    Bump survey version to v3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SURVEY_CONFIG = {
   submissionEndpoint: "https://script.google.com/macros/s/AKfycbxgcXe47MryQeBD0vZujshKARzOarTdhUtneiOdnWQ9OrGcjKVYlSEpY43fJinFEOfmvg/exec",
-  surveyVersion: "2.0.0",
+  surveyVersion: "3.0.0",
   allowLocalPilotWhenEndpointMissing: false
 };
