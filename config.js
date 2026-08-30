18691782cb3dcd33adf615cf085eb7cee418091c
Abdullah X <ax.zephara@gmail.com>
2026-08-30 05:56:22 +0530

    Add survey configuration

---
+++ config.js
@@ -0,0 +1,7 @@
+// Paste the deployed Google Apps Script Web App URL here before collecting responses.
+// Example: https://script.google.com/macros/s/AKfycb.../exec
+window.SURVEY_CONFIG = {
+  submissionEndpoint: "",
+  surveyVersion: "1.0.0",
+  allowLocalPilotWhenEndpointMissing: true
+};
